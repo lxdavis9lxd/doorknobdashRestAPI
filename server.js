77a80d331b6bc1fe81954d8f4aa9188499a68b00
lxdavis9lxd <lxdavis9lxd@gmail.com>
2023-11-20 22:49:21 +0000

    committed

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to MySQL Sequelize application." });
+  res.json({ message: "Welcome to MySQL Sequelize application.   " + corsOptions.origin});
 });
 
 
